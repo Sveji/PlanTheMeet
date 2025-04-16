84b185e71c23eaba836cc0adefccf449c9ced6e0
MrTiNnN <martin.v.velchev@gmail.com>
2025-04-16 10:26:28 +0300

    Making calendar

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,18 +1,48 @@
+import { useEffect, useState } from 'react'
 import './calendar.less'
+import Month from './components/Month'
 
 const Calendar = () => {
+    // Holds the selected date
+    const [selected, setSelected] = useState(new Date())
+
+
+
+    const [month, setMonth] = useState({ index: null, name: null })
+    const [year, setYear] = useState(null)
+
+
+
+    // Gets the name of the given month
+    const getMonthName = (month) => {
+        const months = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ]
+
+        return months[month]
+    }
+
+
+
+    // Sets the values to the selected date on init
+    useEffect(() => {
+        if(selected) {
+            setMonth({
+                index: selected.getMonth(),
+                name: getMonthName(selected.getMonth())
+            })
+            setYear(selected.getFullYear())
+        }
+    }, [selected])
+
+
+
     return (
-        <div className="calendar">
-            {
-                Array.from({ length: 31 }, _ => null).map((date, index) => (
-                    <div className={`date ${index % 2 == 1 ? 'disabled' : null}`}>
-                        <div className="day-box">
-                            <p className="day">{index + 1}</p>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
+        <>
+            <h1>{month.name}</h1>
+            <Month month={month} year={year} />
+        </>
     )
 }
 
