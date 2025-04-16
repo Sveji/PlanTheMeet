300941cac0cacb0e639cf4cadce43c90834d5471
Alexandra Stavreva <alexastavreva@gmail.com>
2025-04-16 11:34:23 +0300

    event card

--- client/src/components/Events/EventCard.jsx
+++ client/src/components/Events/EventCard.jsx
@@ -1,11 +1,24 @@
 import './events.less'
+import IMG from '../../img/jake.jpg'
 
 const EventCard = () => {
 
     return (
+        // <section className='events'>
         <div className='card'>
             <div className='heading'>Sofia SUMMER fest</div>
+            <img src={IMG} className='img' />
+            <div className='description'>
+                <p className='text'>Lorem ipsum dolor sit amet consectetur. Cras erat sed eu in purus. Ultrices fringilla id risus rhoncus sit arcu egestas. Pretium libero leo odio id vulputate mauris. Mollis lacus...</p>
+            </div>
+            <div className='info'>
+                <div>27.04.2025</div>
+                <div className='button'>Add to calendar</div>
+            </div>
+
         </div>
+        // </section>
+
     )
 }
 
