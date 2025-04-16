a0c8a8bd6db22ab1526a3d54a97ead547e10a837
Alexandra Stavreva <alexastavreva@gmail.com>
2025-04-16 11:47:27 +0300

    card

--- client/src/components/Events/EventCard.jsx
+++ client/src/components/Events/EventCard.jsx
@@ -6,14 +6,14 @@ const EventCard = () => {
     return (
         // <section className='events'>
         <div className='card'>
-            <div className='heading'>Sofia SUMMER fest</div>
+            <div className='heading'><h4>Sofia SUMMER fest</h4></div>
             <img src={IMG} className='img' />
             <div className='description'>
                 <p className='text'>Lorem ipsum dolor sit amet consectetur. Cras erat sed eu in purus. Ultrices fringilla id risus rhoncus sit arcu egestas. Pretium libero leo odio id vulputate mauris. Mollis lacus...</p>
             </div>
             <div className='info'>
-                <div>27.04.2025</div>
-                <div className='button'>Add to calendar</div>
+                <div><h4>27.04.2025</h4></div>
+                <div className='button'><h4>Add to calendar</h4></div>
             </div>
 
         </div>
