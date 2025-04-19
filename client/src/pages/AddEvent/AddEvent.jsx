ef36be7ce6230a4ba2dbe43046b6f28617477058
Alexandra Stavreva <alexastavreva@gmail.com>
2025-04-19 20:31:20 +0300

    form

--- client/src/pages/AddEvent/AddEvent.jsx
+++ client/src/pages/AddEvent/AddEvent.jsx
@@ -20,14 +20,49 @@ const AddEvent = () => {
     }, [date])
 
     return (
-        <section className={`section-event ${season}`}>
-            <div className="title-box">
-                <h2>Add an event</h2>
-                <div className="date-box">
-                    {date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`}.{date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`}.{date.getFullYear()}
+        <section className={`section-add-event ${season}`}>
+            <div className="add">
+                <div className="title-box">
+                    <h2>Add an event</h2>
+                    <div className="date-box">
+                        {date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`}.{date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`}.{date.getFullYear()}
+                    </div>
                 </div>
-            </div>
-        </section>
+                <div className="form-box">
+                    <div className="form-container">
+                        <div className="form">
+                            <p className="details">Details</p>
+                            <div className="form-inputs">
+                                <label htmlFor="">Title</label>
+                                <input className="inputs" type="text" />
+                            </div>
+                            <div className="form-inputs">
+                                <label htmlFor="">Description</label>
+                                <textarea name="" id="" className="inputs description"></textarea>
+                            </div>
+                            <div className="info">
+                                <div className="info-inputs">
+                                    <label htmlFor="">Location</label>
+                                    <input type="text" className="inputs" />
+                                </div>
+
+                                <div className="info-inputs">
+                                    <label htmlFor="">Time</label>
+                                    <input type="time" className="inputs" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="form-btn">
+                    <h4>Add to calendar</h4>
+                </div>
+
+
+            </div >
+
+        </section >
     )
 }
 
