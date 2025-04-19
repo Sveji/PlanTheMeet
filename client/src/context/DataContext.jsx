fa542b698038f07b2ee66e2e368ce115254653b9
MrTiNnN <martin.v.velchev@gmail.com>
2025-04-19 17:46:15 +0300

    Fix index error

--- client/src/context/DataContext.jsx
+++ client/src/context/DataContext.jsx
@@ -58,7 +58,7 @@ const DataProvider = ({ children }) => {
         if(month >= 2 && month <= 4) return 'spring'
         if(month >= 5 && month <= 7) return 'summer'
         if(month >= 8 && month <= 10) return 'autumn'
-        if(month == 11 || month == 0 || month.index == 1) return 'winter'
+        if(month == 11 || month == 0 || month == 1) return 'winter'
     }
 
 
